37c843f6bce2159f318dd4b26f8939eb1cb7fc91
annajerofejeva <jerofejeva.anna@yahoo.com>
2014-12-20 16:45:40 +0000

    The entire game workinggit add --a

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,33 +1,66 @@
 $(document).ready(function() {
 
-  var displayBoard = function(player) {
-    $.get('/computer/board', function(data) {
-      for(var i = 0; i < 9; i++){
-        if (data[i] !== null) {
-          $('#'+(i+1)).text(data[i]);
-        };
-      };
-      if (data[9] === null) {
-        $('h3').text("")
-      } else if (data[9] === "winner") {
-        $('h3').text(player+" won!")
-      } else if (data[9] === "draw") {
-        $('h3').text("It's a draw!")
-      }
-    });
-  };
+  // var displayBoard = function(player) {
+  //   $.get('/computer/board', function(data) {
+  //     for(var i = 0; i < 9; i++){
+  //       if (data[i] !== null) {
+  //         $('#'+(i+1)).text(data[i]);
+  //       };
+  //     };
+  //     if (data[9] === null) {
+  //       $('h3').text("")
+  //     } else if (data[9] === "winner") {
+  //         $('#h3').text(player+" won!")
+  //     } else if (data[9] === "draw") {
+  //         $('#h3').text("It's a draw!"); 
+  //     };
+  //   });
+  // };
 
   var recordPlayersChoice = function() {
     var choice = $('#choice').val();
     $.post('/computer/player_result', {choice: choice}).done(function() {
-      displayBoard("You");
-      $('h3').text("Computer is thinking...");
+      $.get('/computer/board', function(data) {
+        for(var i = 0; i < 9; i++){
+          if (data[i] !== null) {
+            $('#'+(i+1)).text(data[i]);
+          };
+        };
+        console.log(data[9]);
+        if (data[9] === null) {
+          $('h4').text("")
+        } else if (data[9] === "winner") {
+            $('#winner').text("You won!")
+        } else if (data[9] === "draw") {
+            $('#winner').text("It's a draw!"); 
+        };
+      }).done(function() {
+        if ($('#winner').text() !== 'You won!') {
+          setTimeout(function (){
+            recordComputersChoice();
+          }, 5000);
+        }
+      });
     });
   };
 
   var recordComputersChoice = function() {
     $.post('/computer/computer_result').done(function() {
-      displayBoard("Computer");
+      $.get('/computer/board', function(data) {
+        for(var i = 0; i < 9; i++){
+          if (data[i] !== null) {
+            $('#'+(i+1)).text(data[i]);
+          };
+        };
+        console.log(data[9]);
+        if (data[9] === null) {
+          $('h4').text("")
+        } else if (data[9] === "winner") {
+            $('#winner').text("Computer won!")
+        } else if (data[9] === "draw") {
+            $('#winner').text("It's a draw!"); 
+        };
+      });
     });
   };
 
@@ -41,9 +74,6 @@ $(document).ready(function() {
 
   $('#submit').on('click', function() {
     recordPlayersChoice();
-    setTimeout(function (){
-      recordComputersChoice();
-    }, 5000);
   });
 
 })
